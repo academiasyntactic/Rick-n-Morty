73dc11a765ffbea6944dadd4975b4bdfb5fd367d
academiasyntactic <syntacticacademia@gmail.com>
2024-06-11 07:29:45 -0500

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
 import CardRick from "./components/CardRick";
@@ -16,9 +16,20 @@ function App() {
     setInformacion(datos);
   };
 
+  useEffect(
+    ()=>{
+      const salsa = async ()=>{
+        const response = await fetch('https://rickandmortyapi.com/api/character');
+        const datos = await response.json();
+        setInformacion(datos)
+      }
+      salsa()
+    },[]
+  )
+
   return (
-    <div>
-      <div className="flex">
+    <div className="flex flex-col flex-wrap justify-center items-center">
+      {/* <div className="flex">
         <Card
           titulo={"Mouse Gamer"}
           imagen={
@@ -31,15 +42,17 @@ function App() {
             "https://www.tauretcomputadores.com/images/products/Product_20221110161325229045307.PL1000-Azul-1.webp"
           }
         />
-      </div>
+      </div> */}
 
       <Navbar setNombre={setNombre} solicitud={solicitud} nombre={nombre} />
-      <div className="flex flex-wrap">
 
-        {informacion && (<h1 className="text-white">El numero de resultados es: {informacion.results.length}</h1>)}
+      {informacion && (<h1 className="text-white">El numero de resultados es: {informacion.results.length}</h1>)}
+      <div className="flex flex-wrap justify-center items-center">
+
+
         {informacion &&
           informacion.results.map((elemento) => (
-            <div>
+            <div key={elemento.id}>
               <img className="rounded-full" src={elemento.image} alt="" />
               <h1 className="text-white">{elemento.name}</h1>
             </div>
